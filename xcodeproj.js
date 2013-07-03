227c627cc06e9b1d5581f50286e590c087024ebd
Thibaud Cummings <thibaud.cummings@gmail.com>
2013-07-03 14:22:39 +0200

    inject code in AppDelegate.m

--- xcodeproj.js
+++ xcodeproj.js
@@ -60,6 +60,65 @@ define(['woodman'], function (woodman) {
     }
 
     /**
+     * Injects source code in AppDelegate.m at various placeholders:
+     *  - imports
+     *  - source
+     *
+     * TODO: prepend rather than replace the placeholders so we can potentially
+     * inject more code.
+     *
+     * @function
+     * @param {function} cb Callback
+     */
+    function injectInApplicationSrc(cb) {
+      // 4 space indent
+      var indent = '    ';
+
+      var imports = [
+        '#import "SHK.h"',
+        '#import "SHKConfiguration.h"',
+        '#import "MySHKConfigurator.h"',
+        '#import "SHKFacebook.h"'
+        // '#import "SHKDropbox.h"',
+        // '#import "SHKGooglePlus.h"',
+        // '#import "EvernoteSDK.h"',
+        // '#import "SHKBuffer.h"',
+        // '#import "PocketAPI.h"'
+      ].join('\n');
+
+      // src code file to inject in src
+      var srcToInjectPath = 'Sharekit/src/AppDelegate.m';
+      runtime.readFileFromAddon(srcToInjectPath, function (err, src) {
+        if (err) {
+          logger.warn('injectInApplicationSrc readFileFromAddon error', err);
+          cb(err);
+        }
+
+        var replaceMap = {
+          '\\/\\*\\*___JOSHFIRE_IMPORT_PLACEHOLDER___\\*\\*\\/': imports,
+          '\\/\\*\\*___JOSHFIRE_SRC_PLACEHOLDER___\\*\\*\\/'   : src
+        };
+
+        // we should do something like 'prepend' so we can inject more...
+
+        logger.log('injectInApplicationSrc replaceMap', replaceMap);
+
+        var fpath = 'Classes/AppDelegate.m';
+        logger.log('injectInApplicationSrc fpath', fpath);
+
+        runtime.multipleReplaceInFile(fpath, replaceMap, function (err) {
+          if (err) {
+            logger.warn('injectInApplicationSrc multipleReplaceInFile error', err);
+          } else {
+            logger.log('injectInApplicationSrc OK!');
+          }
+
+          cb(err);
+        });
+      });
+    }
+
+    /**
      * Do all the fancy business...
      *
      * @function
@@ -132,6 +191,7 @@ define(['woodman'], function (woodman) {
       plugmanInstall,
       copyLibraryToProject,
       customSharekitInstall,
+      injectInApplicationSrc,
       updateSharekitConfig
     ], function (err) {
       if (err) {
