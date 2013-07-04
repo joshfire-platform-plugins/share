5ce2dfaacb9f578da7c30819924cdc53433e4a68
Thibaud Cummings <thibaud.cummings@gmail.com>
2013-07-04 18:38:06 +0200

    added 'share' hook + renamed JS method to window.plugins.share

--- share/www/ShareKitPlugin.js
+++ share/www/ShareKitPlugin.js
@@ -1,6 +1,6 @@
 //
 //  ShareKitPlugin.js
-//  
+//
 //
 //  Created by Erick Camacho on 28/07/11.
 //  MIT Licensed
@@ -14,33 +14,33 @@ function ShareKitPlugin()
 ShareKitPlugin.prototype.share = function(message, url)
 {
 	cordova.exec(null, null, "ShareKitPlugin", "share", [message, url]);
-    
+
 };
 
 
 ShareKitPlugin.prototype.isLoggedToTwitter = function( callback )
 {
-	
+
     cordova.exec(callback, null, "ShareKitPlugin", "isLoggedToTwitter", [] );
 };
 
 ShareKitPlugin.prototype.isLoggedToFacebook = function( callback )
 {
-	
+
     cordova.exec(callback, null, "ShareKitPlugin", "isLoggedToFacebook", [] );
 
 };
 
 ShareKitPlugin.prototype.logoutFromTwitter = function()
 {
-	
+
     cordova.exec(null, null, "ShareKitPlugin", "logoutFromTwitter", [] );
 
 };
 
 ShareKitPlugin.prototype.logoutFromFacebook = function()
 {
-	
+
     cordova.exec(null, null, "ShareKitPlugin", "logoutFromFacebook", [] );
 
 };
@@ -48,30 +48,30 @@ ShareKitPlugin.prototype.logoutFromFacebook = function()
 
 ShareKitPlugin.prototype.facebookConnect = function()
 {
-	
+
     cordova.exec(null, null, "ShareKitPlugin", "facebookConnect", [] );
-    
+
 };
 
 ShareKitPlugin.prototype.shareToFacebook = function( message, url)
 {
-	
+
     cordova.exec(null, null, "ShareKitPlugin", "shareToFacebook", [message, url] );
-    
+
 };
 
 ShareKitPlugin.prototype.shareToTwitter = function( message, url)
 {
-	
+
     cordova.exec(null, null, "ShareKitPlugin", "shareToTwitter", [message, url] );
-    
+
 };
 
 ShareKitPlugin.prototype.shareToMail = function( subject, message)
 {
-	
+
     cordova.exec(null, null, "ShareKitPlugin", "shareToMail", [subject, message] );
-    
+
 };
 
 
@@ -79,13 +79,14 @@ ShareKitPlugin.prototype.shareToMail = function( subject, message)
 
 ShareKitPlugin.install = function()
 {
-    if(!window.plugins)
-    {
-        window.plugins = {};	
+    if (!window.plugins) {
+        window.plugins = {};
     }
 
     window.plugins.shareKit = new ShareKitPlugin();
-    return window.plugins.shareKit;
+    window.plugins.share = window.plugins.shareKit.share;
+
+    return;
 };
 
 cordova.addConstructor(ShareKitPlugin.install);
